6859c1fad9c3709e26260cc9a091174e92feb727
Nora Hajdu <hajdu.nora115@gmail.com>
2026-06-30 13:33:57 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          Adblock detection result:
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
